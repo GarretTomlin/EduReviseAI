f042f54db63dc43b24823fc2d3b5b926f465f57f
garrettomlin <garrettomlin808@gmail.com>
2023-08-26 22:52:19 -0500

    update database config

--- src/config/mikro-orm.config.ts
+++ src/config/mikro-orm.config.ts
@@ -8,7 +8,7 @@ const config: Options = {
   dbName: process.env.DB_NAME,
   type: 'postgresql',
   debug: true,
-  clientUrl: `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  clientUrl: process.env.CLIENT_URL,
   logger: logger.log.bind(logger),
   migrations: {
     path: 'src/database/migrations',
