f522e408efec35c9d890217d9b83d5bcfcdbc79d
garrettomlin <garrettomlin808@gmail.com>
2023-09-09 20:42:14 -0500

    update seeder

--- src/database/seeders/QuestionSeeder.ts
+++ src/database/seeders/QuestionSeeder.ts
@@ -2,7 +2,7 @@ import type { EntityManager } from '@mikro-orm/core';
 import { Seeder } from '@mikro-orm/seeder';
 import { QuestionFactory } from '../factory/question.factory';
 
-export class DatabaseSeeder extends Seeder {
+export class QuestionSeeder extends Seeder {
   async run(em: EntityManager): Promise<void> {
     new QuestionFactory(em).make(10);
   }
