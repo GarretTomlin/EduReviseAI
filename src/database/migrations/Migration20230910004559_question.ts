f4c3e43ed16cfc0ada240f1cdbc04352edca5fc6
garrettomlin <garrettomlin808@gmail.com>
2023-09-09 20:42:02 -0500

    update migrations

--- src/database/migrations/Migration20230910004559_question.ts
+++ src/database/migrations/Migration20230910004559_question.ts
@@ -1,16 +1,9 @@
 import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20230827132938 extends Migration {
+export class Migration20230910004559_question extends Migration {
   async up(): Promise<void> {
     this.addSql(
       'create table "question_bank" ("id" serial primary key, "name" varchar(255) not null, "category" varchar(255) not null, "difficulty" int not null);',
-    );
-
-    this.addSql(
-      'create table "user" ("id" serial primary key, "username" text not null, "password" text not null, "email" text not null default \'example@gmail.com\', "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null);',
-    );
-    this.addSql(
-      'alter table "user" add constraint "user_username_unique" unique ("username");',
     );
 
     this.addSql(
@@ -30,13 +23,7 @@ export class Migration20230827132938 extends Migration {
       'alter table "question" drop constraint "question_question_bank_id_foreign";',
     );
 
-    this.addSql(
-      'alter table "question" drop constraint "question_author_id_foreign";',
-    );
-
     this.addSql('drop table if exists "question_bank" cascade;');
-
-    this.addSql('drop table if exists "user" cascade;');
 
     this.addSql('drop table if exists "question" cascade;');
   }
